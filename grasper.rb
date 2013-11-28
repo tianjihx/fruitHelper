fb7a7e25de2e28067baeb65acfd238694f6df92e
tianjihx <tianjihx@126.com>
2013-11-28 19:29:10 +0800

    添加了exe，添加了识别多水果

--- grasper.rb
+++ grasper.rb
@@ -1,12 +1,12 @@
 ﻿# encoding: utf-8
 fruitTypeRegex = ""
-fruitCountRegex = "\\d+(?=\D?\D?[斤个根只])"
+fruitCountRegex = "(\\d|[一二三四五六七八九])+(?=\D?\D?[斤个根只])"
 fruitUnitRegex = "((?<=\\d)|(?<=\\d\\D)|(?<=\\d\\D\\D))[斤个根只]"
 domRegex = "((博雅)|(疏[桐通]?[园圆元]?[ABCDEabcde])|([静京景晶][园圆元]?)|(6[ABCabc])|([丹][枫风凤峰]?))[ABCDEabcde]?.?.?\\d{3}"
 nameRegex = "(?<=%).+(?=%)"
 orders = Array.new
 messages = Array.new
-fruitMap = {}
+$fruitMap = {}
 fruitTypeRegexFilePath = "data/" + "fruitTypeRegex.txt"
 fruitTypeMapFilePath = "data/" + "fruitTypeMap.rb"
 finPath = "message.txt"
@@ -31,6 +31,20 @@ class Order
 	def addFruit(fruit)
 		@fruits << fruit
 	end
+
+	def toString()
+		outstring = ""
+		outstring += @name
+		outstring += ","
+		outstring += @dom
+		@fruits.each do |fruit|
+			outstring += ','
+			outstring += $fruitMap[fruit.type] + ','
+			outstring += fruit.count + ','
+			outstring += fruit.unit
+		end
+		return outstring
+	end
 end
 
 
@@ -42,7 +56,7 @@ end
 puts "完成"
 print "  ->加载水果映射列表...."
 File.open(fruitTypeMapFilePath, "r:utf-8").each do |line|
-	eval(line)
+	eval("$"+line)
 end
 puts "完成"
 
@@ -72,33 +86,51 @@ print "->发现", messages.count, "条信息\n"
 index = 0
 messages.each do |message|
 	print "->", '#' , messages.index(message)+1, ' : ' 
-	orders.push(Order.new(message.match(nameRegex)))
+	#提取姓名
+	orders.push(Order.new(message.match(nameRegex).to_s))
 	print orders.last.name, ', ' 
-	type = message.match(fruitTypeRegex)
-	print fruitMap[type.to_s], ', ' 
-	count = message.match(fruitCountRegex)
-	print count, ', ' 
-	unit = message.match(fruitUnitRegex)	
-	print unit, ', ' 
-	orders.last.addFruit(Fruit.new(type, count, unit))
-	orders.last.dom = message.match(domRegex)
-	print orders.last.dom, "\n" 
+	#提取宿舍
+	orders.last.dom = message.match(domRegex).to_s
+	print orders.last.dom
+	#提取水果信息
+	while message.match(fruitTypeRegex) != nil do
+		print ', '
+		type = message.match(fruitTypeRegex)
+		print $fruitMap[type.to_s], '' 
+		count = message.match(fruitCountRegex)
+		print count, '' 
+		unit = message.match(fruitUnitRegex)	
+		print unit
+		orders.last.addFruit(Fruit.new(type.to_s, count.to_s, unit.to_s))
+		message.sub!(type.to_s, '')
+		message.sub!(count.to_s, '')
+		message.sub!(unit.to_s, '')
+		#print "\n|debug|-->" + message + "\n"
+	end
+	print "\n"
+	 
 end
 puts "----解析完成-----"
 
 
 
 puts "正在生成orderlist.txt文件"
-fout  = File.open(foutPath, "w+:utf-8")
+begin
+	fout  = File.open(foutPath, "w+:utf-8")
 
-#在控制台输出
-orders.each do |order| 
-	#fout.puts "\##{orders.index(order)}"
-	fout.puts "#{order.name},#{order.dom},#{fruitMap[order.fruits[0].type.to_s]},#{order.fruits[0].count},#{order.fruits[0].unit}"
+	#在控制台输出
+	orders.each do |order| 
+		fout.puts order.toString()
+	end
+	fout.close
+	puts "生成成功"
+	puts "orderlist.txt已被创建"
+rescue Exception => e
+	puts "向orderlist.txt写数据时发生错误"
+	puts "错误："
+	puts e
+	fout.close
 end
-fout.close
 
-puts "生成成功"
-puts "orderlist.txt已被创建"
 puts "按任意键结束..."
 gets
